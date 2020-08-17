0ee34730f4a524b1aa0e7cdb48e984e019742e81
andrewalisa <andrewkalisa@gmail.com>
2020-08-16 21:58:01 -0400

    Testing the concept of .env files. This is what the .env file will look like, then I will readd this to the .gitignore.

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+const searchMovies = async (e) =>{
+    e.preventDefault();
+    console.log(process.env.REACT_APP_SECRET_CODE);
+
+    
+}
+
+
 export default function SearchMovies(){
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query"> Movie Name</label>
             <input className="input" type="text" name="query" htmlFor="query" placeholder="i.e. Toy Story" />
             <button className="button" type="submit">Search</button>

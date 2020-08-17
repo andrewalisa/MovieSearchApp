f2439241121ea6d72ecef0b7cb2b72a37c99f51b
andrewalisa <andrewkalisa@gmail.com>
2020-08-16 20:26:11 -0400

    Create SearchMovies component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import './style.css';
+import SearchMovies from "./SearchMovies";
 
 function App() {
   return (
     <div className="container">
       <div className="title">
         <h1>React Movie Search</h1>
+        <SearchMovies/>
       </div>
     </div>
   );
